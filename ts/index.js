0e3c8f1b21691133d82b5b0976c2f890f16c6735
Adrián Romero <adrianromero@users.noreply.github.com>
2015-09-14 00:36:23 +0200

    Using console parameters

--- ts/index.js
+++ ts/index.js
@@ -2,7 +2,7 @@
 var mymaths = require('./mymaths');
 var _ = require('underscore');
 console.log('Hello TypeScript!!!');
-console.log('Calculate 2 + 2 = ' + mymaths.add(2, 2));
-console.log('Random number between 1 and 100 = ' + _.random(1, 100));
+console.log('Calculate 2 + 2 = %d.', mymaths.add(2, 2));
+console.log('Random number between 1 and 100 = %d.', _.random(1, 100));
 var greet = new mymaths.Greeter('Felipe');
 console.log(greet.greet());
